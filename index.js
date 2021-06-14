43f98ad34b333b3951c82c173a9e04c69b2b641a
mctrivia <squarerootofnegativeone@gmail.com>
2021-06-14 17:20:18 -0500

    compilable

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-require('./lib/syncer');
-require('./lib/api');
+const config=require('./lib/config');
 
+const {port}=config.get('main');
+
+//Print Header
+let lines=["\x1b[5m\x1b[33m  _____  _       _                       _   "," |  __ \\(_)     (_)   /\\                | |  "," | |  | |_  __ _ _   /  \\   ___ ___  ___| |_ "," | |  | | |/ _` | | / /\\ \\ / __/ __|/ _ \\ __|"," | |__| | | (_| | |/ ____ \\\\__ \\__ \\  __/ |_ "," |_____/|_|\\__, |_/_/    \\_\\___/___/\\___|\\__|","            __/ |                            ","  __  __   |___/           _       _         "," |  \\/  |    | |          | |     | |        "," | \\  / | ___| |_ __ _  __| | __ _| |_ __ _  "," | |\\/| |/ _ \\ __/ _` |/ _` |/ _` | __/ _` | "," | |  | |  __/ || (_| | (_| | (_| | || (_| | "," |_|  |_|\\___|\\__\\__,_|\\__,_|\\__,_|\\__\\__,_| ","        / ____|                              ","       | (___   ___ _ ____   _____ _ __      ","        \\___ \\ / _ \\ '__\\ \\ / / _ \\ '__|     ","        ____) |  __/ |   \\ V /  __/ |        ","       |_____/ \\___|_|    \\_/ \\___|_| ",
+    "\x1b[0mThanks for installing DigiAsset Metadata Server.  To view the user interface in a browser enter http://127.0.0.1:"+port+" on first login it is recommended to add a username and password so others can not manipulate this server."];
+for (let line of lines) console.log(line);
+
+require('./lib/api');
+require('./lib/syncer');
--- readme.md
+++ readme.md
@@ -3,24 +3,21 @@
 2. Forward port 4001 to this machine
 3. If planning to run this all the time you may want to set IPFS to run on boot.  In linux I did that by running ```crontab -e``` and adding ```@reboot /usr/local/bin/ipfs daemon```
 
-##Step 2 (Install NodeJS):
-1. Follow instructions at https://nodejs.org/en/download/
+##Step 2 (Install this app):
+1) Copy file from bin folder for your OS to your computer
+2) run it 
 
-##Step 3 (Install this app):
-1) Copy files to a folder
-2) In console run ```npm install```
+## Compile Instructions(for programmers)
+```bash
+#install pkg
+npm install -g pkg
 
-##Step 4 (Run the program):
-##### To run once
-1. in a command line type ```node index.js```
+#if windows user run
+"c:\Program Files\nodejs\nodevars.bat"
 
-##### To run continuously in the background in linux
-1. Install PM2 by following instructions at https://www.vultr.com/docs/how-to-setup-pm2-on-ubuntu-16-04
-2. Add the app by running ```pm2 start index.js --name meta```
-3. To save so it starts on next boot ```pm2 save```
-3. You can check console logs by typing ```pm2 log meta```
+#compile
+pkg .
+```
 
-#Step 5 (View Loaded Meta Data):
-1. Open a web browser 
-2. In the address bar not the search bar enter ```http://127.0.0.1:8090/```
-3. You can now view any meta data that has been stored to your server, mark it as acceptable and delete content that is offensive.
+
+
