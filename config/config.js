2e1be9845163f75e66b5abfb810dd50c331f3e27
mctrivia <squarerootofnegativeone@gmail.com>
2021-06-14 08:29:33 -0500

    added username and password infestructure

--- config/config.js
+++ config/config.js
@@ -8,7 +8,8 @@ module.exports={
     errorDelay:     600000,         //time to delay before retrying if there is an error 10min
     port:           8090,           //page to see what is stored on your server will be available at http://serverip:port/
     ipfsViewer:     ".ipfs.localhost:8080",
-    scanDelay:      15000000,        //delay in ms between scans for new assets.  Default is 1000 blocks
-
+    scanDelay:      15000000,       //delay in ms between scans for new assets.  Default is 1000 blocks
+    sessionLife:    86400000,       //how long sessions last default 24h
+    users:          false,          //login credentials - default no credentials.  when credentials are required Object<{salt:string,hash:String}>
     messaging:      {}              //do not change
 }
